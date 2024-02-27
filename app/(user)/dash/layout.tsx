4f8c0df40114dab04e4ee0284103c6fef9b93953
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-27 11:28:09 -0500

    chore: implement react query client + provider

---
+++ app/(user)/dash/layout.tsx
@@ -0,0 +1,29 @@
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import { Providers } from '@/components/providers';
+import { Toaster } from '@/components/ui/sonner';
+import { checkAuth } from '@/lib/auth/utils';
+import { ClerkProvider } from '@clerk/nextjs';
+
+export default async function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  await checkAuth();
+  return (
+    <>
+      <ClerkProvider>
+        <Providers>
+          <div className="flex h-screen">
+            <main className="flex-1 md:p-8 pt-2 p-8 overflow-y-auto">
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </ClerkProvider>
+
+      <Toaster richColors />
+    </>
+  )
+}
