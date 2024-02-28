9ed0a269d0d5a39f6fa539ca66f57471a60b2184
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-27 23:10:41 -0500

    add user dash to admin dashboard sidebar

--- config/nav.ts
+++ config/nav.ts
@@ -1,5 +1,5 @@
 import { SidebarLink } from "@/components/SidebarItems";
-import { Cog, Globe, HomeIcon } from "lucide-react";
+import { Cog, Globe, HomeIcon, Notebook } from "lucide-react";
 
 type AdditionalLinks = {
   title: string;
@@ -10,6 +10,7 @@ export const defaultLinks: SidebarLink[] = [
   { href: "/dashboard", title: "Home", icon: HomeIcon },
   { href: "/account", title: "Account", icon: Cog },
   { href: "/settings", title: "Settings", icon: Cog },
+  { href: "/dash", title: "User Dashboard", icon: Notebook },
 ];
 
 export const additionalLinks: AdditionalLinks[] = [
@@ -23,6 +24,4 @@ export const additionalLinks: AdditionalLinks[] = [
       },
     ],
   },
-
 ];
-
